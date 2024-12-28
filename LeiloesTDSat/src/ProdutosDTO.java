14e7d246580adb11025ecbf937b91b46aef46ee0
lucas <lucas.sjc87@hotmail.com>
2024-12-27 22:26:55 -0300

    Mensagem de cadastro com sucesso

--- LeiloesTDSat/src/ProdutosDTO.java
+++ LeiloesTDSat/src/ProdutosDTO.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Adm
- */
 public class ProdutosDTO {
     private Integer id;
     private String nome;
@@ -43,6 +35,5 @@ public class ProdutosDTO {
 
     public void setStatus(String status) {
         this.status = status;
-    }
-    
+    }   
 }
