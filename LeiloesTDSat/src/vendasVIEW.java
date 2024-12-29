1e0d71eef7c53adb0ec7d3ebd029f7602b8e48ef
lucas <lucas.sjc87@hotmail.com>
2024-12-28 21:24:32 -0300

    Mostrar produtos vendidos na tela

--- LeiloesTDSat/src/vendasVIEW.java
+++ LeiloesTDSat/src/vendasVIEW.java
@@ -1,8 +1,13 @@
 
+import java.util.ArrayList; 
+import javax.swing.table.DefaultTableModel; 
+import javax.swing.JOptionPane;
+
 public class vendasVIEW extends javax.swing.JFrame {
 
 public vendasVIEW() {
        initComponents();
+       listarProdutosVendidos();
     }
 
     @SuppressWarnings("unchecked")
@@ -11,6 +16,8 @@ public vendasVIEW() {
 
         jPanel1 = new javax.swing.JPanel();
         btnvoltar = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tabelaVendas = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -22,21 +29,40 @@ public vendasVIEW() {
             }
         });
 
+        tabelaVendas.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "ID", "Nome", "Valor", "Status"
+            }
+        ));
+        jScrollPane1.setViewportView(tabelaVendas);
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(38, 38, 38)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(61, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(424, Short.MAX_VALUE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(btnvoltar)
-                .addGap(34, 34, 34))
+                .addGap(30, 30, 30))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(409, Short.MAX_VALUE)
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
                 .addComponent(btnvoltar)
-                .addGap(32, 32, 32))
+                .addContainerGap(20, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -56,8 +82,28 @@ public vendasVIEW() {
     private void btnvoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnvoltarActionPerformed
         listagemVIEW tela = new listagemVIEW(); 
         tela.setVisible(true);
+        dispose();
     }//GEN-LAST:event_btnvoltarActionPerformed
 
+    private void listarProdutosVendidos() { 
+        try { 
+        ProdutosDAO produtosdao = new ProdutosDAO(); 
+        DefaultTableModel model = (DefaultTableModel) tabelaVendas.getModel(); 
+        model.setNumRows(0); 
+        ArrayList<ProdutosDTO> listagem = produtosdao.listarProdutosVendidos(); 
+        for (ProdutosDTO produto : listagem) { 
+        model.addRow(new Object[]{ 
+        produto.getId(), 
+        produto.getNome(), 
+        produto.getValor(), 
+        produto.getStatus() 
+        }); 
+        } 
+        } catch (Exception e) { 
+        JOptionPane.showMessageDialog(null, "Erro ao listar produtos vendidos: " + e.getMessage()); 
+        } 
+        }  
+    
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -93,5 +139,7 @@ public vendasVIEW() {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnvoltar;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tabelaVendas;
     // End of variables declaration//GEN-END:variables
 }
